52d08d41f2d5f43b8b5a039cf0a263ace94e34bd
segcore <44293604+segcore@users.noreply.github.com>
2024-04-07 16:15:44 +0800

    Build selector initial version with Makefile and CMake support

---
+++ README.md
@@ -0,0 +1,41 @@
+# Build Selector for Neovim
+
+Utility to intelligently calculate likely `makeprg` targets to easily
+select a build target.
+
+## Overview
+
+Detects files such as `Makefile` and `CMakeLists.txt` and builds up options
+that you may want to use as `makeprg`.
+
+### Makefile
+
+Detects `Makefile` and `makefile`, and adds options `make -f <FILE>`
+
+### CMake
+
+Detects `CMakeLists.txt` and then searches for `build*/CMakeCache.txt` and adds
+`cmake --build <PATH> --parallel` for each of them.
+
+## Setup
+
+Lazy:
+
+```
+{ "segcore/build-selector", opts = {} }
+```
+
+## Usage
+
+```
+:BuildSelector
+```
+
+Pops up the default ui-select menu with selections, e.g.
+
+```
+1. make -f Makefile
+2. cmake --build build-gcc-debug --parallel
+3. cmake --build build-gcc-release --parallel
+4. cmake --build build-rpi-release --parallel
+```
---
+++ lua/build-selector/init.lua
@@ -0,0 +1,83 @@
+local M = {}
+
+local function getcwd()
+  return vim.uv.cwd()
+end
+
+M.find_files = function(files, cwd)
+  cwd = cwd or getcwd()
+  local result = vim.fs.find(files , {
+    upward = true,
+    type = 'file',
+    limit = math.huge,
+    stop = vim.uv.os_homedir(),
+    path = cwd,
+  })
+  return result
+end
+
+M.makefiles = function(cwd)
+  return M.find_files({'Makefile', 'makefile' }, cwd)
+end
+
+M.cmakelists = function(cwd)
+  return M.find_files({'CMakeLists.txt'}, cwd)
+end
+
+M.devcontainers = function(cwd)
+  return {}
+end
+
+M.simplify = function(path, cwd)
+  return require('plenary').path:new(path):make_relative()
+end
+
+M.choice_makefile = function(file)
+  return "make -f " .. M.simplify(file)
+end
+
+M.choice_cmake = function(file)
+  -- Find a build directory next to this file
+  local dirname = vim.fs.dirname(file)
+  for subdir_name, type in vim.fs.dir(dirname) do
+    if vim.startswith(subdir_name, "build") and type == 'directory' then
+      local cmake_cache = vim.fs.joinpath(dirname, subdir_name, "CMakeCache.txt")
+      if vim.fn.filereadable(cmake_cache) then
+        return 'cmake --build ' .. M.simplify(vim.fs.joinpath(dirname, subdir_name)) .. " --parallel"
+      end
+    end
+  end
+end
+
+M.make_choices = function(cwd)
+  local result = {}
+  for _, file in pairs(M.makefiles(cwd)) do
+    table.insert(result, M.choice_makefile(file))
+  end
+  for _, file in pairs(M.cmakelists(cwd)) do
+    local entry = M.choice_cmake(file)
+    if entry then table.insert(result, entry) end
+  end
+  return result
+end
+
+M.choose = function()
+  local choices = M.make_choices()
+  if choices and #choices > 0 then
+    vim.ui.select(choices, { prompt = 'Set make program to::' }, function(item, index)
+      if item then
+        print("makeprg set to " .. item)
+        vim.opt_local.makeprg = item
+      end
+    end)
+  else
+    print("Could not find anything to build")
+  end
+end
+
+
+M.setup = function()
+  vim.api.nvim_create_user_command('BuildSelector', M.choose, { desc = 'Select a makeprg' })
+end
+
+return M
